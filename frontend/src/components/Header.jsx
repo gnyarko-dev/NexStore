b54fcaec0658b8b24f845661bae858f83306e090
Arabian619 <www.arabiandollar152@gmail.com>
2024-09-13 13:35:15 +0000

    Button feature added-landing page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 // import { BiWorld } from "react-icons/bi";
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineUser } from "react-icons/ai";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 function Header() {
   return (
@@ -12,10 +20,25 @@ function Header() {
         <div className='flex items-center pr-3 font-semibold'>
             <p className=' px-3 py-1 rounded-full hover:bg-gray-300 duration-100 ease-out mx-4'>Add Store</p>
             {/* <BiWorld className='flex items-center mx-4 gap-1' /> */}
-            <div className="flex items-center border px-3 py-2 rounded-full gap-1 shadow-lg shadow-gray-200 hover:bg-gray-300 duration-100 ease-out">
+            {/* <div className="flex items-center border px-3 py-2 rounded-full gap-1 shadow-lg shadow-gray-200 hover:bg-gray-300 duration-100 ease-out">
                 <FiMenu className='text-[19]' />
                 <AiOutlineUser className='text-[19]' />
-            </div>
+            </div> */}
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+              <div className="flex items-center border px-3 py-2 rounded-full gap-1 shadow-lg shadow-gray-200 hover:bg-gray-300 duration-100 ease-out">
+                <FiMenu className='text-[19]' />
+                <AiOutlineUser className='text-[19]' />
+              </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>Sign Up</DropdownMenuItem>
+                <DropdownMenuItem>Log In</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Add Store</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
         </div>
     </div>
   )
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div>
         <div className="flex flex-col items-center p-10 py-20 gap-4 h-[600px] w-full bg-[#eef0fc]">
-            <h2 className='text-lg font-bold text-[50px]'>Discover the nearest store around you!</h2>
+            <h2 className='text-lg font-bold in-text'>Discover the nearest store around you!</h2>
             <h2>Pharmacies, grocery stores, mobile money vendors, salons, car washes, any type of store you need to visit</h2>
 
             <Search />
