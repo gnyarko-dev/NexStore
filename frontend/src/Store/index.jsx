3fc82f977a373404999d27b622a1cee37a18d8ca
Arabian619 <www.arabiandollar152@gmail.com>
2024-09-21 18:13:55 +0000

    Store Inputfield

--- frontend/src/Store/index.jsx
+++ frontend/src/Store/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import StoreHeader from './StoreHeader';
 import Sidebar from '@/components/ui/Sidebar';
 import '../components/Header';
+import storeDetails from './storeDetail.json';
+import InputField from './components/InputField';
 
 function AddStore() {
   return (
@@ -9,60 +11,30 @@ function AddStore() {
       <div className='sticky-header'>
        <StoreHeader/>
       </div>
-      
       <Sidebar/>
-      <div className='grid w-full h-full pl-[350px]'>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       <p>homepage</p>
-       
+      <div className='grid w-full h-full pl-[300px]'>
+        <div className='px:10 md:px-20 my-10'>
+          <h2 className='font-bold text-4xl'>Store</h2>
+          <form className='p-10 border rounded-xl mt-10'>
+            {/* Store details */}
+            <div>
+              <h2 className='font-medium text-xl mb-6'>Store details</h2>
+              <div className='grid gap-5'>
+                {storeDetails.storeDetails.map((item,index)=>(
+                  <div key={index}>
+                    <label className='text-sm'>{item?.label} {item.required&&<span className='text-red-500'>*</span>}</label>
+                    {item.fieldType=='text'||item.fieldType=='number'?<InputField item={item} />:null}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Product / Service */}
+
+            {/* Inventory */}
+
+          </form>
+        </div>
       </div>
     </div>
   )
